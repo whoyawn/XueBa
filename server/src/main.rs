1052fa601bac2367c9c537261f3f762676d97d36
Benji Pelletier <pelletierbenji@gmail.com>
2025-04-05 13:09:23 -0400

    Switch Rocket for Axum

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,23 @@
-#[macro_use] extern crate rocket;
+use axum::{
+    routing::get,
+    Router,
+    extract::Path,
+};
+use dotenv::dotenv;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+#[tokio::main]
+async fn main() {
+    // Build our application with a route
+    let app = Router::new()
+        .route("/track/:spotify_id", get(get_track));
+
+    // Run it with hyper on localhost:3000
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    println!("Server running on http://localhost:3000");
+    axum::serve(listener, app).await.unwrap();
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+async fn get_track(Path(spotify_id): Path<String>) -> String {
+    dotenv().ok();
+    format!("Hello, {}", spotify_id)
 }
